792f89220c33aa6bac7e8fd032200fb30ca45148
Michael Easter <codetojoy@gmail.com>
2016-05-24 10:30:21 -0300

    incremental change as part of a larger frenzy of activity

--- exampleC/dao_client/DAO_Client.groovy
+++ exampleC/dao_client/DAO_Client.groovy
@@ -3,9 +3,9 @@ import com.acme.flagship.dao.*
 
 def dao = new AccountDao()
         
-List<Account> accounts = dao.findAccountsByStatus(Account.CANCEL)
+List<Account> accounts = dao.findAccountsByStatus(Account.ACTIVE)
 
-System.out.println("cancelled accounts :")
+System.out.println("active accounts :")
 
 for (Account a : accounts) {
     System.out.println("account id: " + a.getAccountId() + " username: " + a.getUsername())
