d3bb80af681f76d7c5b4f136410726f588e6fe9c
Michael Easter <codetojoy@gmail.com>
2016-05-24 08:25:41 -0300

    incremental change as part of a larger frenzy of activity

--- exampleB/v1_groovy/run.sh
+++ exampleB/v1_groovy/run.sh
@@ -8,10 +8,12 @@ COMPILE_CLASSPATH=./lib/commons-logging-1.2.jar:$COMPILE_CLASSPATH
 COMPILE_CLASSPATH=./lib/commons-codec-1.9.jar:$COMPILE_CLASSPATH
 
 echo "compiling ..."
+
 groovyc -cp $COMPILE_CLASSPATH RESTClient.groovy
 
 RUNTIME_CLASSPATH=$GROOVY_HOME/embeddable/groovy-all-2.4.5.jar:$COMPILE_CLASSPATH
 
 echo "running ..."
+
 java -cp $RUNTIME_CLASSPATH RESTClient
 
