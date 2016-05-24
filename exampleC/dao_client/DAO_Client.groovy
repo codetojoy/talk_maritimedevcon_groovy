c20c84847c55a054a16abbcfef26799f33da642c
Michael Easter <codetojoy@gmail.com>
2016-05-24 08:29:16 -0300

    incremental change as part of a larger frenzy of activity

--- exampleC/dao_client/DAO_Client.groovy
+++ exampleC/dao_client/DAO_Client.groovy
@@ -5,8 +5,10 @@ def dao = new AccountDao()
         
 List<Account> accounts = dao.findAccountsByStatus(Account.CANCEL)
 
+System.out.println("cancelled accounts :")
+
 for (Account a : accounts) {
-    System.out.println("TRACER  : " + a.getUsername())
+    System.out.println("account id: " + a.getAccountId() + " username: " + a.getUsername())
 }
 
 
--- exampleC/dao_client/run.sh
+++ exampleC/dao_client/run.sh
@@ -2,6 +2,7 @@
 COMPILE_CLASSPATH=../acme_dao/dist/acme-flagship-dao-1.0.jar
 
 echo "compiling ..."
+
 groovyc -cp $COMPILE_CLASSPATH DAO_Client.groovy
 
 RUNTIME_CLASSPATH=.
@@ -10,5 +11,6 @@ RUNTIME_CLASSPATH=$HSQLDB_HOME/lib/hsqldb.jar:$RUNTIME_CLASSPATH
 RUNTIME_CLASSPATH=$GROOVY_HOME/embeddable/groovy-all-2.4.5.jar:$RUNTIME_CLASSPATH
 
 echo "running ..."
+
 java -cp $RUNTIME_CLASSPATH DAO_Client
 
